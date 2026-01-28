c71a0603169f081548c1e1099738e6cb6ec87835
milochen0418 <milo_chen@mail2000.com.tw>
2026-01-29 01:11:23 +0800

    fix warning from rx.config

--- rxconfig.py
+++ rxconfig.py
@@ -1,3 +1,9 @@
 import reflex as rx
 
-config = rx.Config(app_name="dicom_data_explorer_intro", plugins=[rx.plugins.TailwindV3Plugin()])
+config = rx.Config(
+	app_name="dicom_data_explorer_intro",
+	plugins=[
+		rx.plugins.TailwindV3Plugin(),
+		rx.plugins.sitemap.SitemapPlugin(),
+	],
+)
